237f30b3368bd6a27bd406f4027003b362f79b69
Alex Kesling <alex.kesling@gmail.com>
2014-08-10 23:20:56 -0400

    Resolved some concurrency issues, brought up more.

--- src/interchange/hub.go
+++ src/interchange/hub.go
@@ -23,40 +23,73 @@ type subscriber struct {
 // there will be five topic nodes: one for the root and each of foo and bar...
 // and one for each of baz and qux.
 type topicNode struct {
-    Name       string
-    Children   *topicNode[]
-    subs       subscriber[]
+    Name     string
+    Children *topicNode[]
+    subs     subscriber[]
+    end      chan bool
+    Comm     chan Message
 }
 
-func (t *topicNode) Push(message Message) {
-    for i := subs {
-        go func() {
-            subs[i].Sink <- message
-        }
-    }
+func (t *topicNode) Start() {
+    // This must be set to 1 or destruct behaves incorrectly
+    t.end = make(chan bool, 1)
 
-    for i := t.children {
-        go t.children[i].Push(message)
+    go func() {
+        for {
+            select {
+            case signal := <-t.end:
+                break
+            case  message := <-Comm:
+                for i := subs {
+                    // TODO(akesling): make it safe to spin this off as a
+                    // go routine.  Currently destruction can race with an
+                    // asynchronous send.
+                    t.subs[i].Sink <- message
+                }
+
+                for i := t.children {
+                    t.children[i].Comm <- message
+                }
+            }
+        }
+        // Apparently we're being shut down.
+
+        // Let's shut down our children.
+        for i := t.children {
+            t.children[i].destruct()
+        }
+
+        // And close our subscribers.
+        for i := t.subs {
+            close(t.subs[i].Sink)
+        }
+    }()
+}
+
+func (t *topicNode) destruct() {
+    select {
+    case t.end <- true:
+    default:
+        log.Error('Multiple call to topicNode.destruct().')
     }
 }
 
 // A hub plays matchmaker between publishers and subscribers.
-type hub struct {
-    topicTrie topicNode*
-}
+//
+// It is the root of topic Trie
+type hub topicNode
 
 // NewHub builds a hub.
 func NewHub() *hub {
-    return new hub{new topicNode{'.'}}
+    h := *hub(new topicNode{'.'})
+    h.Start()
+    return interchange
 }
 
 func (h *hub) Publish(topic string, message Message) error {
-    base, err := findAndMergeTopic(topic)
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    go base.Push(message)
+    go func() {
+        h.Comm <- message
+    }()
 }
 
 func (h *hub) Subscribe(topic string) (secondsLease int, chan<- Message, error) {
@@ -65,8 +98,4 @@ func (h *hub) Subscribe(topic string) (secondsLease int, chan<- Message, error) 
 // NewClient creates a new client for the given hub.
 func (h *hub) NewClient() *Client {
     return Client(h)
-}
-
-// findAndMergeTopic finds existing topics and merges new ones into the trie.
-func (h *hub) findAndMergeTopic(topic string[]) (*topicNode, error) {
 }
